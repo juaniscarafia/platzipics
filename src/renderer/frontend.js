b4b6190e5649c464b5651e07b313ac03c96e186f
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2022-06-10 11:32:17 -0300

    Evento de selección de imagenes al clickear un thumb

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,3 +1,19 @@
 window.addEventListener('load', () => {
-    
+    addImageEvents();
 });
+
+function addImageEvents() {
+    const thumbs = document.querySelectorAll('li.list-group-item');
+
+    thumbs.forEach(imagen => {
+        imagen.addEventListener('click', function () {
+            changeImage(this);
+        });
+    });
+}
+
+function changeImage(node){
+    document.querySelector('li.selected').classList.remove('selected');
+    node.classList.add('selected');
+    document.getElementById('image-displayed').src = node.querySelector('img').src;
+}
