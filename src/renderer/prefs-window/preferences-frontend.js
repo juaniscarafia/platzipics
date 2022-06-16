9b9169e75c207d8b5f392c5de9633b0a8b424a7e
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2022-06-16 09:03:28 -0300

    guardado user y password (not work)

--- src/renderer/prefs-window/preferences-frontend.js
+++ src/renderer/prefs-window/preferences-frontend.js
@@ -1,6 +1,16 @@
-import { remote } from 'electron';
+import { remote, ipcRenderer } from 'electron';
+import settings from 'electron-app-settings';
+
 window.addEventListener('load', () => {
     cancelButton();
+    saveButton();
+
+    if (settings.has('cloudup-user')) {
+        document.getElementById('cloudup-user').value = settings.get('cloudup-user');
+    }
+    if (settings.has('cloudup.password')) {
+        document.getElementById('cloudup-passwd').value = settings.get('cloudup-password');
+    }
 });
 
 function cancelButton(){
@@ -8,5 +18,27 @@ function cancelButton(){
     cancelButton.addEventListener('click', () => {
         const prefsWindow = remote.getCurrentWindow();
         prefsWindow.close();
+    });
+}
+
+function saveButton(){
+    const saveButton = document.getElementById('save-button');
+
+    const prefsForm = document.getElementById('preferences-form');
+
+    saveButton.addEventListener('click', () => {
+        if (prefsForm.reportValidity()) {
+            settings.set('cloudup.user', document.getElementById('cloudup-user').value);
+            settings.set('cloudup.password', document.getElementById('cloudup-passwd').value);
+            const prefsWindow = remote.getCurrentWindow();
+            prefsWindow.close();
+        }
+        else {
+            ipcRenderer.send('show-dialog', {
+                type: 'error', 
+                title: 'Platzipics', 
+                message: 'Por favor complete los campos requeridos.'
+            });
+        }
     });
 }
